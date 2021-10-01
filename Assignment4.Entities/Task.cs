6ce6a21b77b72285a7bc45dfa8f6ea7871977d5b
ehou <ehou@itu.dk>
2021-10-01 13:52:54 +0200

    Setup  (not done)

--- Assignment4.Entities/Task.cs
+++ Assignment4.Entities/Task.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Assignment4.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
 namespace Assignment4.Entities
 {
     public class Task
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public User? AssignedTo { get; set; }
+        public string? Description { get; set; }
+        public State State { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+        public void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                        .Property(e => e.State)
+                        .HasConversion(new EnumToStringConverter<State>());
+        }
     }
 }
